d98402739384f44c51763dfc818c656603b76fb2
dagnym <jesse.williams@hotmail.com>
2024-10-18 15:26:19 -0400

    adding oidc for battlenet

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from "@aws-amplify/backend";
 
 /**
  * Define and configure your auth resource
@@ -7,5 +7,24 @@ import { defineAuth } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      oidc: [
+        {
+          name: "BattleNet",
+          clientId: secret("BATTLENET_CLIENT_ID"),
+          clientSecret: secret("BATTLENET_CLIENT_SECRET"),
+          issuerUrl: "https://us.battle.net/oauth",
+          scopes: ["openid", "wow.profile"],
+        },
+      ],
+      logoutUrls: [
+        "http://localhost:3000/",
+        "https://main.dlvgd9rcsj33x.amplifyapp.com",
+      ],
+      callbackUrls: [
+        "http://localhost:3000",
+        "https://main.dlvgd9rcsj33x.amplifyapp.com",
+      ],
+    },
   },
 });
