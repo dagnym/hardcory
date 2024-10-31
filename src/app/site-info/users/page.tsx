b672bc731e1a08d4c1e40e72b49da8d91e8a905f
dagnym <jesse.williams@hotmail.com>
2024-10-31 17:09:45 -0400

    updated user page attempt

--- src/app/site-info/users/page.tsx
+++ src/app/site-info/users/page.tsx
@@ -1,31 +1,20 @@
+"use server";
+
 import { db } from "@/db/drizzle";
 import { users } from "@/db/schema";
 
+import UserList from "@/components/UserList";
+
 const SiteUsersPage = async () => {
   const siteUsers = await db.select().from(users);
-
+  console.log("siteUsers: ", siteUsers);
   return (
     <div className="w-full h-full p-20">
       <a href="/" className="border px-2 py-1 rounded-sm">
         Home
       </a>
-      <div className="grid grid-cols-4  mt-20">
-        {siteUsers.map((user) => (
-          <form
-            key={user.id}
-            className="flex flex-col p-4 bg-gray-600 border rounded-sm border-blue-900 bg-opacity-30 m-auto mt-20 space-y-2"
-          >
-            <div className="flex flex-col self-center">
-              <h2 className="mb-4">{user?.username}</h2>
-              <img
-                alt=""
-                src={user?.profilepicture || ""}
-                className="w-60 h-60"
-              />
-            </div>
-          </form>
-        ))}
-      </div>
+
+      <UserList users={siteUsers} />
     </div>
   );
 };
---
+++ src/components/UserList.js
@@ -0,0 +1,25 @@
+"use client";
+
+const UserList = ({ users }) => {
+  return (
+    <div className="grid grid-cols-4  mt-20">
+      {users.map((user) => (
+        <form
+          key={user.id}
+          className="flex flex-col p-4 bg-gray-600 border rounded-sm border-blue-900 bg-opacity-30 m-auto mt-20 space-y-2"
+        >
+          <div className="flex flex-col self-center">
+            <h2 className="mb-4">{user?.username}</h2>
+            <img
+              alt=""
+              src={user?.profilepicture || ""}
+              className="w-60 h-60"
+            />
+          </div>
+        </form>
+      ))}
+    </div>
+  );
+};
+
+export default UserList;
