45bbb5ffe08bf2e941b3ca62166a2c7e6125dd8a
dagnym <jesse.williams@hotmail.com>
2024-11-01 15:23:29 -0400

    changed users page to client

---
+++ src/app/api/neon/get_users/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { users } from "@/db/schema";
+import { db } from "@/db/drizzle";
+
+export async function GET() {
+  try {
+    const existingUsers = await db.select().from(users);
+    return NextResponse.json({ users: existingUsers });
+  } catch (err) {
+    console.log("error in get users api: ", err);
+    return NextResponse.json({ data: "failed" });
+  }
+}
--- src/app/site-info/users/page.tsx
+++ src/app/site-info/users/page.tsx
@@ -1,15 +1,18 @@
-export const metadata = {
-  revalidate: 0,
-};
-
-import { db } from "@/db/drizzle";
-import { users } from "@/db/schema";
+"use client";
 
 import UserList from "@/components/UserList";
+import { useEffect, useState } from "react";
 
-const SiteUsersPage = async () => {
-  const siteUsers = await db.select().from(users);
-
+const SiteUsersPage = () => {
+  const [siteUsers, setSiteUsers] = useState([]);
+  useEffect(() => {
+    const getUsers = async () => {
+      const response = await fetch("/api/neon/get_users");
+      const data = await response.json();
+      setSiteUsers(data.users);
+    };
+    getUsers();
+  }, []);
   return (
     <div className="w-full h-full p-20">
       <a href="/" className="border px-2 py-1 rounded-sm">
